cd6376cb9cdf7e9044a80a7bd719a55e22c97315
Lucas Kepler <lucas@Laptop-de-Lucas.local>
2024-05-24 20:25:48 -0300

    inser select

---
+++ src/components/Select/Budgets/SpeedCopy/index.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import { View } from 'react-native';
+import RNPickerSelect from 'react-native-picker-select';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+
+export const SpeedCopy = () => {
+  const [selectedValue, setSelectedValue] = useState('Português');
+
+  return (
+    <View  className='bg-inputColor flex-row w-96 mr-20 ml-1 h-14  rounded-md items-center justify-between flex-1'>
+        <View className='items-center'> 
+      </View>
+      <View> 
+      <RNPickerSelect
+        onValueChange={(value) => {
+          setSelectedValue(value);
+          console.log(value);
+        }}
+        items={[
+          { label: 'Português', value: 'Português' },
+          { label: 'Inglês', value: 'Inglês' }
+        ]}
+        value={selectedValue}
+        useNativeAndroidPickerStyle={false} // Use custom styles for Android
+        placeholder={{}} // No placeholder
+      />
+      </View>
+      <View className='mr-5'>
+      <FontAwesomeIcon icon={faChevronDown} />
+      </View>
+     
+    </View>
+  );
+};
+
+
+
--- src/pages/Budgets/CreateBudgets/SpecsBudget/index.tsx
+++ src/pages/Budgets/CreateBudgets/SpecsBudget/index.tsx
@@ -1,25 +1,69 @@
-import { Text, TouchableOpacity, View } from "react-native";
+import { Text, View } from "react-native";
 import HeaderOnlyBack from "../../../../components/Headers/HeaderOnlyBack";
 import SearchBar from "../../../../components/TextInput";
 import { PrintResolution } from "../../../../components/Select/Budgets/PrintResolution";
+import { SpeedProcessor } from "../../../../components/Select/Budgets/SpeedProcessor";
 import { SelectClients } from "../../../../components/Select/Clients/SelectClients";
+import { Memory } from "../../../../components/Select/Budgets/Memory";
+import { ControlPainel } from "../../../../components/Select/Budgets/ControlPainel";
+import { ScrollView } from "react-native-gesture-handler";
+import { FrontBackPrint } from "../../../../components/Select/Budgets/FrontBackPrint";
+import { Nfc } from "../../../../components/Select/Budgets/Nfc";
+import { SpeedCopy } from "../../../../components/Select/Budgets/SpeedCopy";
+import { CopyResolution } from "../../../../components/Select/Budgets/CopyResolution";
+import { Tooner } from "../../../../components/Select/Budgets/Tooner";
 
 export default function SpecsBudget() {
   return (
-    <View className="mt-20">
+    <View className="flex-1 mt-20">
       <HeaderOnlyBack title={"Especificações"} />
       <View className="ml-5 mt-10">
         <SearchBar />
       </View>
-      <View className="bg-inputColor w-96 h-14 rounded-md items-center ml-5 justify-between flex-1">
-        <View className="h-20 mt-12 ml-14">
-            <View>
-            <Text className="font-bold ml-8 text-xl">Cliente</Text>
-            </View>
-          <SelectClients />
-        
+      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+        <View className=" w-full rounded-md items-center flex-1 p-4">
+          <View className="mt-8 w-full">
+            <Text className="font-bold text-xl mb-1">Cliente</Text>
+            <SelectClients />
+          </View>
+          <View className="mt-8 w-full">
+            <Text className="font-bold text-lg mb-1">Resolução de Impressão</Text>
+            <PrintResolution />
+          </View>
+          <View className="mt-8 w-full">
+            <Text className="font-bold text-lg mb-1">Velocidade do Processador</Text>
+            <SpeedProcessor />
+          </View>
+          <View className="mt-8 w-full">
+            <Text className="font-bold text-lg mb-1">Memória</Text>
+            <Memory />
+          </View>
+          <View className="mt-8 w-full mb-5">
+            <Text className="font-bold text-lg mb-1">Painel de Controle</Text>
+            <ControlPainel />
+          </View>
+          <View className="mt-8 w-full mb-5">
+            <Text className="font-bold text-lg mb-1">Impressão frente e verso automatica</Text>
+            <FrontBackPrint />
+          </View>
+          <View className="mt-8 w-full mb-5">
+            <Text className="font-bold text-lg mb-1">NFC</Text>
+            <Nfc />
+          </View>
+          <View className="mt-8 w-full mb-5">
+            <Text className="font-bold text-lg mb-1">Velocidade de Cópia</Text>
+            <SpeedCopy />
+          </View>
+          <View className="mt-8 w-full mb-5">
+            <Text className="font-bold text-lg mb-1">Resolução de Cópia</Text>
+            <CopyResolution />
+          </View>
+          <View className="mt-8 w-full mb-5">
+            <Text className="font-bold text-lg mb-1">Tooner Para</Text>
+            <Tooner />
+          </View>
         </View>
-      </View>
+      </ScrollView>
     </View>
   );
 }
