04c1fa48e8aa176667f2d856173fff32ae08b8ea
Lucas Kepler <lucas@Laptop-de-Lucas.local>
2024-05-26 00:41:54 -0300

    login changes

--- src/components/LoginInputs/index.tsx
+++ src/components/LoginInputs/index.tsx
@@ -1,45 +1,56 @@
 import { Text, View, TextInput } from "react-native";
 import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
-import { StyledComponent } from "nativewind";
+import { faEnvelope, faEye, faEyeSlash, faLock } from "@fortawesome/free-solid-svg-icons";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import { useNavigation } from '@react-navigation/native';
-
-
-
+import { StyledComponent } from "nativewind";
 
 export default function EmailInput() {
     const [email, setEmail] = useState('');
 
     return (
-        <View className="bg-inputColor border-2 onFocus:border-inputColor border-gray-200 mt-10 w-80 h-12 rounded-md items-center justify-center">
+        <View className="bg-inputColor border-2 onFocus:border-inputColor  border-gray-200 mt-10 w-80 h-12 rounded-md flex-row items-center px-3">
+            <View className="bg-white w-7 h-7 rounded-full items-center justify-center">
+                <FontAwesomeIcon icon={faEnvelope} style={{color: "#6359e7"}} />
+            </View>
             <TextInput
                 onChangeText={setEmail}
                 value={email}
                 placeholder="Digite seu e-mail"
                 keyboardType="email-address"
                 autoCapitalize="none"
-                className="w-full items-center justify-center text-center text-black font-bold"
+                className="flex-1 ml-2 text-center text-black font-bold"
             />
         </View>
     );
 }
 
-
 export function PasswordInput() {
     const [password, setPassword] = useState('');
+    const [secureText, setSecureText] = useState(true);
+
+    function toggleShowPassword() {
+        setSecureText(!secureText);
+    }
 
     return (
-        <View className="bg-inputColor border-2 onFocus:border-inputColor border-gray-200 mt-5 w-80 h-12 rounded-md items-center justify-center">
+        <View className="bg-inputColor border-2 onFocus:border-inputColor border-gray-200 mt-5 w-80 h-12 rounded-md flex-row items-center px-3">
+            <View className="bg-white w-7 h-7 rounded-full items-center justify-center">
+                <FontAwesomeIcon icon={faLock} style={{color: "#6359e7"}} />
+            </View>
             <TextInput
                 onChangeText={setPassword}
                 value={password}
                 placeholder="Digite sua senha"
-                keyboardType="visible-password"
+                keyboardType="default"
                 autoCapitalize="none"
-                className="w-full items-center justify-center text-center text-black font-bold"
-                secureTextEntry={true}
+                className="flex-1 ml-2 text-center text-black font-bold"
+                secureTextEntry={secureText}
             />
+            <TouchableOpacity onPress={toggleShowPassword}>
+                <FontAwesomeIcon icon={secureText ? faEye : faEyeSlash} style={{color: "#6359e7"}} />
+            </TouchableOpacity>
         </View>
     );
 }
@@ -49,7 +60,7 @@ export function LoginButton() {
     return (
         <View className="justify-end mt-10">
             <StyledComponent component={TouchableOpacity} >
-                <TouchableOpacity className="h-14 bg-primary w-80 rounded-md items-center justify-center"  onPress={() => {navigation.navigate('Services');}}>
+                <TouchableOpacity className="h-14 bg-primary w-80 rounded-md items-center justify-center" onPress={() => {navigation.navigate('Services');}}>
                     <Text className="text-white font-medium font-Principal text-md">
                         Login
                     </Text>
