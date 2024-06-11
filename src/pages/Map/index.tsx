19a55ba2c5b3e3c9cf44d424c08a2708a8089406
Lucas Augusto Kepler <lucaskepler991@gmail.com>
2024-06-11 20:10:25 -0300

    reserves changes

--- src/pages/Map/index.tsx
+++ src/pages/Map/index.tsx
@@ -1,7 +1,11 @@
-import { Text } from "react-native";
+import { Text, View } from "react-native";
+import MapView from "react-native-maps";
+import HeaderOnlyBack from "../../components/Headers/HeaderOnlyBack";
 
 export default function Clients() {
     return (
-        <Text>Clients</Text>
+        <View className="mt-10">
+            <HeaderOnlyBack title={"Mapa"} />
+        </View>
     );
 }
--- src/pages/Reserves/index.tsx
+++ src/pages/Reserves/index.tsx
@@ -1,7 +1,28 @@
-import { Text } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
+import HeaderBackPlusReserves from "../../components/Headers/HeaderBackPlusReserves";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
 
-export default function Clients() {
-    return (
-        <Text>Clients</Text>
-    );
+export default function Reserves() {
+  return (
+    <View className="mt-10">
+      <HeaderBackPlusReserves title={"Reservas"} />
+      <View>
+        <View className="items-center bg-inputColor mt-5 w-96 ml-5 rounded-lg h-28 justify-center">
+          <View className="flex-row w-96 mb-3 ml-5 items-center">
+            <View className="mr-4">
+              <Text>01</Text>
+            </View>
+            <View className="ml-5">
+              <Text className="font-bold text-lg">Barril</Text>
+            </View>
+          </View>
+          <View className="flex-row w-96 mb-2 ml-32 items-center">
+            <FontAwesomeIcon icon={faUser} style={{ color: "gray" }} />
+            <Text className="ml-2">Cliente</Text>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
 }
