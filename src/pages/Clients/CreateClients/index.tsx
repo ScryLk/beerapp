23c42130c01c677e8dce0149f4ba0ae62a44b897
Lucas Augusto Kepler <lucaskepler991@gmail.com>
2024-06-16 02:36:54 -0300

    create clients change

--- src/pages/Clients/CreateClients/index.tsx
+++ src/pages/Clients/CreateClients/index.tsx
@@ -1,37 +1,55 @@
 import React, { useState } from "react";
 import HeaderOnlyBack from "../../../components/Headers/HeaderOnlyBack";
 import { Text, View, TouchableOpacity, TextInput, Alert } from "react-native";
-import { CreateButton } from "../../../components/Buttons";
-import MapView, { Marker } from "react-native-maps";
-import { useNavigation } from "@react-navigation/native";
 import { StyledComponent } from "nativewind";
 
 export default function CreateClients() {
   const [name, setName] = useState("");
   const [number, setNumber] = useState("");
   const [address, setAddress] = useState("");
-  const [markerPosition, setMarkerPosition] = useState({
-    latitude: -28.2901,
-    longitude: -53.4909,
-  });
-
-  const handleMapPress = (e) => {
-    setMarkerPosition({
-      latitude: e.nativeEvent.coordinate.latitude,
-      longitude: e.nativeEvent.coordinate.longitude,
-    });
-  };
 
   function sendCoordinates() {
     console.log(address);
   }
 
   const validateData = () => {
-    if (name === "" || number === "" || address === null) {
+    if (name === "" || number === "" || address === "") {
       Alert.alert("Erro", "Todos os campos devem ser preenchidos.");
       return false;
     }
-    return (Alert.alert("Cliente Cadastrado com sucesso"))
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    const raw = JSON.stringify({
+      "nome": name,
+      "numero": number,
+      "endereco": address
+    });
+
+    const requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow"
+    };
+
+    fetch("http://192.168.0.155:3000/clientes/", requestOptions)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok ' + response.statusText);
+        }
+        return response.text();
+      })
+      .then((result) => {
+        Alert.alert("Sucesso", "Cliente Cadastrado com sucesso");
+        console.log(result);
+      })
+      .catch((error) => {
+        Alert.alert("Erro", "Erro ao cadastrar cliente");
+        console.error(error);
+      });
+
+    return true;
   };
 
   return (
@@ -69,21 +87,17 @@ export default function CreateClients() {
       </View>
       <View>
         <Text className="text-xl font-bold ml-5 mt-4">Endereço</Text>
-        <View className="flex justify-center items-center">
-          <MapView
-            className="w-96 h-60"
-            initialRegion={{
-              latitude: -28.2901,
-              longitude: -53.4909,
-              latitudeDelta: 0.0922,
-              longitudeDelta: 0.0421,
-            }}
-            onPress={handleMapPress}
-            onTouchEndCapture={sendCoordinates}
-            onMarkerSelect={() => setAddress(markerPosition)}
-          >
-            <Marker coordinate={markerPosition} />
-          </MapView>
+        <View className="flex-row justify-center items-center">
+          <View className="bg-inputColor border-2  border-gray-200 w-96 h-12 rounded-md flex-row items-center px-2">
+            <TextInput
+              placeholder="Endereço"
+              keyboardType="default"
+              autoCapitalize="none"
+              value={address}
+              onChangeText={setAddress}
+              className="flex-1 pl-2 text-black font-bold"
+            />
+          </View>
         </View>
       </View>
       <View className="flex-row justify-center items-center">
@@ -91,9 +105,9 @@ export default function CreateClients() {
           <StyledComponent component={TouchableOpacity}>
             <TouchableOpacity
               className="h-14 bg-clientsColor w-96 rounded-md items-center justify-center"
-              onPress={() => validateData()}
+              onPress={validateData}
             >
-              <Text className="text-white font-medium font-Principal text-md"> 
+              <Text className="text-white font-medium font-Principal text-md">
                 Criar
               </Text>
             </TouchableOpacity>
